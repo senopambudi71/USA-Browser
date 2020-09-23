79744fa9cba725f22b635bf235a89276e88bfb85
utami <utami4711@gmail.com>
2020-09-23 11:02:27 -0700

    remove ads

--- app/src/main/java/ons/dsi/usabrowser/SplashActivity.kt
+++ app/src/main/java/ons/dsi/usabrowser/SplashActivity.kt
@@ -1,16 +1,16 @@
 package ons.dsi.usabrowser
 
+import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
 import android.os.Handler
 import android.preference.PreferenceManager
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.ads.AdListener
-import com.google.android.gms.ads.AdRequest
 import com.google.android.gms.ads.InterstitialAd
-import com.google.android.gms.ads.MobileAds
 import com.startapp.sdk.adsbase.StartAppAd
+import kotlinx.android.synthetic.main.activity_splash.*
 import java.util.*
 
 
@@ -22,14 +22,18 @@ class SplashActivity : AppCompatActivity() {
     private var runable: Runnable? = null
     var handler = Handler()
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-//        MobileAds.initialize(this, "ca-app-pub-2322131950454521~2034818350")
+
+        if (isNetworkAvailable(this)){
+            rotateloading.start()
+        }
+
 
         val prefs = PreferenceManager.getDefaultSharedPreferences(this)
         val lastopen = prefs.getInt("lastopen", 0)
-
         val editor = prefs.edit()
         editor.putInt("lastopen", Date().hours)
         editor.apply()
@@ -39,7 +43,20 @@ class SplashActivity : AppCompatActivity() {
             startActivity(i)
             return
         }
-        startAppAd.loadAd()
+    }
+
+    fun isNetworkAvailable(context: Context): Boolean {
+        val connectivity = context
+                .getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        if (null != connectivity) {
+            val info = connectivity.activeNetworkInfo
+            if (null != info && info.isConnected) {
+                if (info.state == NetworkInfo.State.CONNECTED) {
+                    return true
+                }
+            }
+        }
+        return false
     }
 
     fun startDelay(time: Long){
