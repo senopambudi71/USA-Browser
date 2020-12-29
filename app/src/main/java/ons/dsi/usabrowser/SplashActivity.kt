989b23d1f4cd64182e15f0f3db9f84236e8271c6
utami <utami4711@gmail.com>
2020-12-29 13:49:36 +0700

    fixing flow notif

--- app/src/main/java/ons/dsi/usabrowser/SplashActivity.kt
+++ app/src/main/java/ons/dsi/usabrowser/SplashActivity.kt
@@ -46,25 +46,18 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
         AudienceNetworkAds.initialize(this)
 
-        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
-        val lastopen = prefs.getInt("lastopen", 0)
-        val editor = prefs.edit()
-        editor.putInt("lastopen", Date().hours)
-        editor.apply()
-
-//        if (lastopen == Date().hours) {
-////           cekUpdateApp()
-//            showSplash()
-//        }
-//        else {
-            cekUpdateApp()
-//            facebookAdsLoad()
-//        }
+        cekUpdateApp()
     }
 
     private fun cekUpdateApp() {
         val queue = Volley.newRequestQueue(this)
         val url: String = "https://raw.githubusercontent.com/triutami11/triutami11.github.io/main/obrowser.json"
+
+        val prefs = PreferenceManager.getDefaultSharedPreferences(this)
+        val lastopen = prefs.getInt("lastopen", 0)
+        val editor = prefs.edit()
+        editor.putInt("lastopen", Date().hours)
+        editor.apply()
 
         val stringReq = StringRequest(Request.Method.GET, url,
             Response.Listener<String> {response ->
@@ -82,36 +75,49 @@ class SplashActivity : AppCompatActivity() {
 
                 if(infovCode < version){
                     Log.i("Respons", "New version O Browser is available on Google play store. Please Updated soon !!")
-                    runable = Runnable {
-                        val builder = AlertDialog.Builder(this)
-                        builder.setTitle("Information Update")
-                        builder.setMessage("New version App is available on Google play store. Please Update Now !")
+                    if (lastopen == Date().hours) {
+                        runable = Runnable {
+                            val builder = AlertDialog.Builder(this)
+                            builder.setTitle("Information Update")
+                            builder.setMessage("New version App is available on Google play store. Please Update Now !")
 
-                        builder.setPositiveButton(android.R.string.yes) { dialog, which ->
-                            val intent = Intent(Intent.ACTION_VIEW)
-                            intent.data = Uri.parse("https://play.google.com/store/apps/details?id=com.ybrowser.master")
-                            startActivity(intent)
+                            builder.setPositiveButton(android.R.string.yes) { dialog, which ->
+                                val intent = Intent(Intent.ACTION_VIEW)
+                                intent.data = Uri.parse("https://play.google.com/store/apps/details?id=com.ybrowser.master")
+                                startActivity(intent)
+                            }
+
+                            builder.setNegativeButton(android.R.string.no) { dialog, which ->
+                                Log.i("Respons", "cancel update and load ads from server")
+                                showSplash()
+                            }
+
+                            builder.show()
                         }
+                        startDelay(3000)
 
-                        builder.setNegativeButton(android.R.string.no) { dialog, which ->
-                            Log.i("Respons", "cancel update and load ads from server")
-                            facebookAdsLoadServer(interstitialIdObject)
-                        }
-
-                        builder.show()
                     }
-                    startDelay(3000)
+                    else {
+                        facebookAdsLoadServer(interstitialIdObject)
+                    }
 
                 }else{
                     Log.i("Respons", "nothing updated in play store and load ads from server")
-                    facebookAdsLoadServer(interstitialIdObject)
+                    if (lastopen == Date().hours) {
+                        showSplash()
+                    }else{
+                        facebookAdsLoadServer(interstitialIdObject)
+                    }
+
                 }
-
-
             },
             Response.ErrorListener {
                 Log.e("ResponsError", "No Data and load ads from local")
-                facebookAdsLoad()
+                if (lastopen == Date().hours) {
+                    showSplash()
+                }else{
+                    facebookAdsLoadServer("")
+                }
             })
         queue.add(stringReq)
     }
@@ -136,7 +142,7 @@ class SplashActivity : AppCompatActivity() {
     private fun facebookAdsLoadServer(idInterstitial: String) {
         Log.i("Ads", idInterstitial)
     //facebookAds
-        if (idInterstitial != null){
+        if (idInterstitial != ""){
             interstitialAd = InterstitialAd(this, idInterstitial)
         }else{
             interstitialAd = InterstitialAd(this, getString(R.string.id_interstitial))
@@ -201,68 +207,6 @@ class SplashActivity : AppCompatActivity() {
 
     }
 
-    private fun facebookAdsLoad() {
-        //facebookAds
-         interstitialAd = InterstitialAd(this, getString(R.string.id_interstitial))
-//        AdSettings.addTestDevice("04c1e69f-881f-443e-b419-6b0056114f53")
-        interstitialAd!!.setAdListener(object : InterstitialAdListener {
-            override fun onInterstitialDisplayed(ad: Ad?) {}
-            override fun onInterstitialDismissed(ad: Ad?) {
-//                finish()
-                startActivity(Intent(this@SplashActivity, MainActivity::class.java))
-            }
-            override fun onError(ad: Ad?, adError: AdError?) {
-                // Interstitial ad is loaded and ready to be displayed
-//                Log.e(TAG, "Interstitial ad is error to display")
-//                // Show the ad
-//                if(!adRespons)
-//                    return
-//
-//                handler.removeCallbacks(runable)
-//                handler.post(runable)
-                Log.i("Ads", "onAdError ${adError?.errorMessage}")
-            }
-            override fun onAdLoaded(ad: Ad?) {
-                // Interstitial ad is loaded and ready to be displayed
-//                Log.d(TAG, "Interstitial ad is loaded and ready to be displayed!")
-//                // Show the ad
-                if(!adRespons)
-                    return
-
-                handler.removeCallbacks(runable)
-                handler.post(runable)
-//                interstitialAd!!.show()
-                Log.i("Ads", "onAdsLoad")
-            }
-
-            override fun onAdClicked(ad: Ad?) {
-                // Ad clicked callback
-                Log.d(TAG, "Interstitial ad clicked!")
-            }
-
-            override fun onLoggingImpression(ad: Ad?) {
-                // Ad impression logged callback
-                Log.d(TAG, "Interstitial ad impression logged!")
-            }
-        })
-        interstitialAd!!.loadAd()
-
-        runable = Runnable {
-            adRespons = false
-            if (interstitialAd!!.isAdLoaded){
-                Log.i("Ads", "Adds Show")
-//                opened this comment if you want show this ads
-                interstitialAd!!.show()
-            }else{
-                Log.d("Ads", "Ads not show")
-                val i = Intent(this@SplashActivity, MainActivity::class.java)
-                startActivity(i)
-            }
-        }
-
-        startDelay(4000)
-
-    }
     fun isNetworkAvailable(context: Context): Boolean {
         val connectivity = context
                 .getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
